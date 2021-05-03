dc4c59bb6829c9d3c19d24d85d01dcd7c87f995d
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-05-03 15:53:48 +0800

    api user create route

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,52 +1,38 @@
-const mongoose = require('mongoose')
-const express = require('express')
-const router = express.Router()
-const validator = require('../validators/user.validator')
-const jwt = require('jsonwebtoken')
-const { SECRET } = require('../models/jwt')
-const bcrypt = require('bcrypt')
-const saltRounds = 10
+const { Router } = require('express')
+const UserModel = require('../models/user.model')
+const UserValidator = require('../validators/user.validator')
+const route = Router()
 
-const {User} = require('../models/user.model')
-
-router.post('/user/register', async (req, res) => {
+route.post('/user/create', async (req, res) => {
   try {
-    const { username, password, confirmPassword, email } = req.body
-    await validator.register.validateAsync({ username, password, confirmPassword, email })
-    const existingUser = await User.findOne({ username });
-    if(existingUser){
-      return res.json({ err: 'user already exists' }).status(401);
-    }
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    const registerUser = new User({
+    const {
+      email,
       username,
-      password: hashedPassword,
-      email
+      password,
+      confirmPassword
+    } = req.body
+
+    await UserValidator.register.validateAsync({
+      email,
+      username,
+      password,
+      confirmPassword
     })
-    await registerUser.save();
+
+    const newUser = new UserModel({
+      email,
+      username,
+      password,
+      confirmPassword
+    })
+
+    await newUser.save()
     res.sendStatus(201)
+
   } catch (error) {
     console.log(error)
     res.sendStatus(400)
   }
 })
 
-router.post('/user/login', async (req, res) => {
-  try {
-    const { username, password, } = req.body
-    await validator.login.validateAsync({ username, password })
-    const { _id, password: userPassword } = await User.findOne({ username });
-
-    const match = await bcrypt.compare(password, userPassword);
-    if (match) {
-      const token = await jwt.sign({ username, _id }, SECRET);
-      return res.json({ token });
-    }
-  res.status(401);
-  } catch (error) {
-    console.log(error)
-    res.sendStatus(400)
-  }
-})
-
-module.exports = router
+module.exports = route
