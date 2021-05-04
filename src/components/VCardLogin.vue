5cfe7de5b91c8572b5ac564d7b651861afaefdc1
RJ Reroma <reromarj@gmail.com>
2021-05-04 15:31:22 +0800

    Wrap in v-card

--- src/components/VCardLogin.vue
+++ src/components/VCardLogin.vue
@@ -1,26 +1,29 @@
 <template>
-  <v-card-text>
-    <v-form v-model="valid" ref="form">
-      <v-text-field 
-        label="Username" 
-        type="text" 
-        :rules="rulesMixin.username"
-      ></v-text-field>
-      <v-text-field 
-        label="Password" 
-        type="password" 
-        :rules="rulesMixin.password"
-      ></v-text-field>
-    </v-form>
+  <v-card>
+    <v-card-text>
+      <v-form v-model="valid" ref="form">
+        <v-text-field 
+          label="Username" 
+          type="text" 
+          :rules="rulesMixin.username"
+        ></v-text-field>
+        <v-text-field 
+          label="Password" 
+          type="password" 
+          :rules="rulesMixin.password"
+        ></v-text-field>
+      </v-form>
+    </v-card-text>
     <v-card-actions>
       <v-btn 
         color="primary" 
         :loading="loading" 
         :disabled="!valid"
         @click="handleLogin"
-      >Submit</v-btn>
+      >Submit
+      </v-btn>
     </v-card-actions>
-  </v-card-text>
+  </v-card>
 </template>
 
 <script>
