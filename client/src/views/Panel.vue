2160d768665a8d8b5647268d62b8bddf625bed33
RJ Reroma <reromarj@gmail.com>
2021-05-13 19:45:32 +0800

    panel component vue

--- client/src/views/Panel.vue
+++ client/src/views/Panel.vue
@@ -1,5 +1,19 @@
 <template>
-  <v-card>
-
-  </v-card>
+  <v-container fill-height>
+    <v-row>
+      <v-col cols="12">
+        <VCardPic2Prod class="mx-auto" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+import VCardPic2Prod from '@/components/VCardPic2Prod'
+export default {
+  name: 'Panel',
+  components: {
+    VCardPic2Prod
+  }
+}
+</script>
