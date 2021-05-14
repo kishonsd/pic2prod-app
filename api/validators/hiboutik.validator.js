08e4d0db5a8bd67b6e0cd1802387c3792ecfcf72
RJ Reroma <reromarj@gmail.com>
2021-05-15 00:44:26 +0800

    api hiboutik validator

---
+++ api/validators/hiboutik.validator.js
@@ -0,0 +1,32 @@
+const Joi = require('joi')
+
+const product_name = Joi.string()
+    .alphanum()
+    .required()
+
+const product_barcode = Joi.string()
+    .alphanum()
+    .required()
+
+const product_brand = Joi.number()
+    .integer()
+    .required()
+
+const product_supplier = Joi.number()
+    .integer()
+    .required()
+
+const product_price = Joi.string()
+    .alphanum()
+    .required()
+
+
+module.exports = {
+  create: Joi.object({
+    product_name,
+    product_barcode,
+    product_brand,
+    product_supplier,
+    product_price
+  })
+}
