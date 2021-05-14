b6f8dece8ecbec74e0a66eba4931e85770ff9e0d
RJ Reroma <reromarj@gmail.com>
2021-05-15 00:44:04 +0800

    get hiboutik posts route

---
+++ api/routes/hiboutik.routes.js
@@ -0,0 +1,33 @@
+const validator = require('../validators/hiboutik.validator')
+const pic2prod = require('../utils/pic2prod')
+const PostModel = require('../models/hiboutik.model')
+const express = require('express')
+const route = express.Router()
+
+route.get('/hiboutik/all', async (req, res) => {
+    try {
+      const items = await PostModel.find()
+      res.json(items)
+    }
+    catch(error) {
+      console.log(error)
+      res.sendStatus(400)
+    }
+  })
+  
+
+route.post('/hiboutik/post', async (req, res) => {
+    try {
+        const { product_name, product_barcode, product_brand, product_supplier, product_price } = req.body
+        await pic2prod.save(
+          { product_name, product_barcode, product_brand, product_supplier, product_price },
+          validator.create,
+          model
+        ).then(() => res.sendStatus(201))
+    } catch (error) {
+    console.log(error)
+    res.sendStatus(400)
+    }
+})
+
+module.exports = route
