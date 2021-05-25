839f271292be7f71be45af9229eb5be1230e2436
RJ Reroma <reromarj@gmail.com>
2021-05-25 20:53:34 +0800

    get hiboutik posts function route

--- api/routes/hiboutik.routes.js
+++ api/routes/hiboutik.routes.js
@@ -3,12 +3,6 @@ const express = require('express')
 const route = express.Router()
 const FormData = require('form-data')
 
-const host = process.env.HIBOUTIK_HOST
-const auth = {
-  username: process.env.HIBOUTIK_USER,
-  password: process.env.HIBOUTIK_PASS
-}
-
 function convert (data) {
   const form = new FormData()
   Object.keys(data)
@@ -20,7 +14,7 @@ route.get('/pic2prod/hiboutik', async (req, res) => {
   axios({
     url: `${host}/products`,
     method: 'GET',
-    auth
+    auth: req.body.auth
   })
   .then((response) => {
     res.json(response.data)
@@ -32,6 +26,11 @@ route.get('/pic2prod/hiboutik', async (req, res) => {
   
 
 route.post('/pic2prod/hiboutik', async (req, res) => {
+  const auth = {
+    username: req.body.username,
+    password: req.body.password
+  }
+  const host = req.body.host
   const product = {
     product_model: req.body.product_model,
     product_barcode: req.body.product_barcode,
@@ -51,7 +50,6 @@ route.post('/pic2prod/hiboutik', async (req, res) => {
   })
   .then((response) => {
     res.sendStatus(201)
-    console.log(form)
   })
   .catch((err) => {
     console.log(err)
