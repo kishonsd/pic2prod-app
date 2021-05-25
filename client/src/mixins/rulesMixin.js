0ee40987a7a9926da208702d8682560076864aac
RJ Reroma <reromarj@gmail.com>
2021-05-25 14:30:54 +0800

    client rules mixin update

--- client/src/mixins/rulesMixin.js
+++ client/src/mixins/rulesMixin.js
@@ -68,6 +68,67 @@ const mixin = Vue.mixin({
           v => !!v || 'Field is required',
         ],
       },
+      eBay: {
+        skuP: [
+          v => !!v || 'Field is required',
+        ],
+        title: [
+          v => !!v || 'Field is required',
+        ],
+        description: [
+          v => !!v || 'Field is required',
+        ],
+        condition: [
+          v => !!v || 'Field is required',
+        ],
+        quantity: [
+          v => !!v || 'Field is required',
+          v => /^[0-9]*$/i.test(v) || 'Field must be numeric',
+        ],
+        marketplaceId: [
+          v => !!v || 'Field is required',
+        ],
+        format: [
+          v => !!v || 'Field is required',
+        ],
+        listingDescription: [
+          v => !!v || 'Field is required',
+        ],
+        availableQuantity: [
+          v => !!v || 'Field is required',
+          v => /^[0-9]*$/i.test(v) || 'Field must be numeric',
+        ],
+        quantityLimitPerBuyer: [
+          v => !!v || 'Field is required',
+          v => /^[0-9]*$/i.test(v) || 'Field must be numeric',
+        ],
+        value: [
+          v => !!v || 'Field is required',
+          v => /^[0-9]*$/i.test(v) || 'Field must be numeric',
+        ],
+        currency: [
+          v => !!v || 'Field is required',
+        ],
+        fulfillmentPolicyId: [
+          v => !!v || 'Field is required',
+        ],
+        paymentPolicyId: [
+          v => !!v || 'Field is required',
+        ],
+        returnPolicyId: [
+          v => !!v || 'Field is required',
+        ],
+        categoryId: [
+          v => !!v || 'Field is required',
+        ],
+        merchantLocationKey: [
+          v => !!v || 'Field is required',
+        ],
+        vatPercentage: [
+          v => !!v || 'Field is required',
+          v => /^[0-9]*$/i.test(v) || 'Field must be numeric',
+        ],
+      },
     }
   })
 })
