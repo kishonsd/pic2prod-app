69401a1494e442b9f75c27d9a899250f06071d07
Rj Reroma <reromarj@gmail.com>
2021-06-14 20:28:52 +0800

    docker setup

--- client/src/utils/serverUtil.js
+++ client/src/utils/serverUtil.js
@@ -1,14 +1,30 @@
 import axios from 'axios'
+import store from '@/store'
 
+/**
+ *
+ *
+ * @param {*} path
+ * @param {*} options
+ * @return {*} 
+ */
 async function serverUtil (path, options) {
   return axios({
-    url: process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:5000/api/' + path,
+    url: process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:8081/api/' + path,
+    method: 'GET',
+    withCredentials: true,
     ...options
   })
-    .then(response => response.data ? response.data : false)
+    .then(response => {
+      if(response.data) {
+        store.dispatch('toast/success', `${response.status} ${response.statusText}`)
+        return response.data
+      }
+    })
     .catch(err => {
-      if (err.response) alert(`${err.response.statusCode} ${err.response.statusText}`)
-      else alert('500 Server Internal Server Error')
+      console.error(err)
+      if (err.response) store.dispatch('toast/error', `${err.response.statusCode} ${err.response.statusText}`)
+      else store.dispatch('toast/error', '500 Server Internal Server Error')
     })
 }
 
