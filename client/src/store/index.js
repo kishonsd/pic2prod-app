8568ae9886d42bef8683c1fb9fc052bcb532f1bd
RJ Reroma <reromarj@gmail.com>
2021-06-15 08:53:45 +0200

    client store with users and toast

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,12 +2,17 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate';
 import user from './modules/user'
+import users from './modules/users'
+import toast from './modules/toast'
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    user
+    user,
+    users,
+    toast
   },
   plugins: [createPersistedState()]
 })
