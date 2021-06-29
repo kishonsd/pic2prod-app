e327a08fddc8c6e9e72c0146265dd70d9b98d105
Rj Reroma <reromarj@gmail.com>
2021-06-30 00:05:29 +0800

    client v-card-login change layouts and style

--- client/src/components/VCardLogin.vue
+++ client/src/components/VCardLogin.vue
@@ -1,33 +1,58 @@
 <template>
-  <v-card>
-    <v-card-text>
-      <v-form v-model="valid" ref="form">
-        <v-text-field 
-          label="Username" 
-          v-model="form.username"
-          type="text" 
-          required
-          :rules="rulesMixin.username"
-        ></v-text-field>
-        <v-text-field 
-          label="Password" 
-          v-model="form.password"
-          type="password" 
-          required
-          :rules="rulesMixin.password"
-        ></v-text-field>
-      </v-form>
-    </v-card-text>
-    <v-card-actions>
-      <v-btn 
-        color="primary" 
-        :loading="loading" 
-        :disabled="!valid"
-        @click="handleLogin"
-      >Submit
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-space-between>
+      <v-flex sm12 md6 offset-md3>
+        <v-card elevation="4" light tag="section">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Admin Login</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form v-model="valid" ref="form">
+              <v-text-field 
+                prepend-icon="mdi-account"
+                label="Username" 
+                v-model="form.username"
+                type="text" 
+                required
+                :rules="rulesMixin.username"
+              ></v-text-field>
+              <v-text-field 
+                prepend-icon="mdi-lock"
+                label="Password" 
+                v-model="form.password"
+                type="password" 
+                required
+                :rules="rulesMixin.password"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn 
+              color="primary" 
+              :loading="loading" 
+              :disabled="!valid"
+              @click="handleLogin"
+            >
+            <v-icon left>mdi-lock-open-check</v-icon>
+            Login
+            </v-btn>
+          </v-card-actions>
+          <v-card-actions>
+            Don't have an account?
+            <v-btn 
+              class="ml-3"
+              :loading="loading" 
+              light
+              @click="goToRegister"
+            >
+            <v-icon>mdi-pencil</v-icon>
+            Sign Up
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -48,6 +73,10 @@ export default {
     valid: true,
   }),
   methods: {
+    goToRegister() {
+      this.$router.push('/register')
+    },
+
     async handleLogin() {
       this.loading = true
       if(this.$refs.form.validate()) {
