9dc51f42c2b33ebbaca78cb5dadc97be74a19929
Rj Reroma <reromarj@gmail.com>
2021-06-30 00:05:37 +0800

    client v-card-register change layouts and style

--- client/src/components/VCardRegister.vue
+++ client/src/components/VCardRegister.vue
@@ -1,47 +1,67 @@
 <template>
-  <v-card max-width="500">
-    <v-card-title class="orange--text">
-      Register
-    </v-card-title>
-    <v-card-text>
-      <v-form v-model="valid"
-              ref="form">
-        <v-text-field label="Email"
-                      type="email"
-                      v-model="form.email"
-                      :rules="rulesMixin.email" />
+  <v-container fluid fill-height>
+    <v-layout align-center justify-space-between>
+      <v-flex sm12 md6 offset-md3>
+        <v-card elevation="4" light tag="section">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title class="orange--text">Register form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form v-model="valid"
+                    ref="form">
+              <v-text-field label="Email"
+                            prepend-icon="mdi-email"
+                            type="email"
+                            v-model="form.email"
+                            :rules="rulesMixin.email" />
 
-        <v-text-field label="Username"
-                      type="username"
-                      v-model="form.username"
-                      :rules="rulesMixin.username" />
+              <v-text-field label="Username"
+                            prepend-icon="mdi-account"
+                            type="username"
+                            v-model="form.username"
+                            :rules="rulesMixin.username" />
 
-        <v-text-field label="Password"
-                      type="password"
-                      v-model="form.password"
-                      :rules="rulesMixin.password" />
+              <v-text-field label="Password"
+                            prepend-icon="mdi-lock"
+                            type="password"
+                            v-model="form.password"
+                            :rules="rulesMixin.password" />
 
-        <v-text-field label="Confirm Password"
-                      type="password"
-                      v-model="form.confirmPassword"
-                      :rules="[v => v === form.password || 'Password does not match']" />
+              <v-text-field label="Confirm Password"
+                            prepend-icon="mdi-lock-open-check"
+                            type="password"
+                            v-model="form.confirmPassword"
+                            :rules="[v => v === form.password || 'Password does not match']" />
 
-      </v-form>
-    </v-card-text>
+            </v-form>
+          </v-card-text>
 
-    <v-card-actions>
-      <v-btn color="primary"
-             :disabled="!valid"
-             :loading="loading"
-             @click="handleRegister">
-        Submit
-      </v-btn>
-    </v-card-actions>
+          <v-card-actions>
+            <v-btn color="primary"
+                  :disabled="!valid"
+                  :loading="loading"
+                  @click="handleRegister">
+              <v-icon>mdi-pencil</v-icon>
+              Submit
+            </v-btn>
+          </v-card-actions>
 
-    <v-card-actions>
-      <router-link to="/">Already have an account?</router-link>
-    </v-card-actions>
-  </v-card>
+          <v-card-actions>
+            Already have an account?
+            <v-btn 
+              class="ml-3"
+              :loading="loading" 
+              light
+              @click="goToLogin"
+            >
+            <v-icon>mdi-lock-open-check</v-icon>
+            Sign In
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>   
 
 <script>
@@ -60,6 +80,10 @@ export default {
   }),
 
   methods: {
+    goToLogin() {
+      this.$router.push('/')
+    },
+
     async handleRegister () {
       this.loading = true
       await serverUtil('user/create', {
