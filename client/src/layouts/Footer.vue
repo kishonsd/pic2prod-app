acdffedc29239ba4c47867433a7cd4c7ea324d8d
Rj Reroma <reromarj@gmail.com>
2021-07-01 18:21:12 +0800

    client footer layout

---
+++ client/src/layouts/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-footer
+    app
+    color="primary lighten-1"
+    padless
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-col
+        class="deep-purple py-4 text-center white--text"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Pic2Prod</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer',
+}
+</script>
