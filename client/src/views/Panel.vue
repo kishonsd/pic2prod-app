eaaa0654ea0a7f2e1c661f76326339a9fa2f4bb9
Rj Reroma <reromarj@gmail.com>
2021-07-01 18:22:40 +0800

    client panel added apbar and footer

--- client/src/views/Panel.vue
+++ client/src/views/Panel.vue
@@ -1,20 +1,26 @@
 <template>
   <v-container fill-height>
+    <app-bar />
     <v-row>
       <v-col cols="12">
         <VCardPic2Prod class="mx-auto" />
       </v-col>
     </v-row>
+    <Footer />
   </v-container>
 </template>
 
 <script>
 import VCardPic2Prod from '@/components/VCardPic2Prod'
 import serverUtil from '../utils/serverUtil'
+import AppBar from '../layouts/AppBar.vue'
+import Footer from '../layouts/Footer.vue'
 export default {
   name: 'Panel',
   components: {
-    VCardPic2Prod
+    VCardPic2Prod,
+    AppBar,
+    Footer
   },
   mounted() {
     this.getUserData()
