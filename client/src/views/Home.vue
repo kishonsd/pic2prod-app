d6a95371fe1d6679f42b42a3b9f1b7862e3f60ad
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-07-12 20:58:54 +0800

    template home view

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,15 +1,45 @@
 <template>
-  <hello-world />
+  <v-main class="primary">
+    <v-container fill-height>
+      <v-row>
+        <v-col md="4">
+          <v-img src="@/assets/Online shopping.png"></v-img>
+        </v-col>
+        <v-col cols="12"
+               md="8">
+          <v-card class="mx-auto"
+                  color="transparent"
+                  flat
+                  shaped
+                  dark>
+            <v-card-text>
+              <h1 class="display-3 font-weight-bold white--text">
+                Amazing company deserve amazing software.
+              </h1>
+            </v-card-text>
+            <v-card-text>
+              <h2>
+                Millions of happy users work better with our integrated Apps.
+              </h2>
+            </v-card-text>
+            <v-card-actions class="mt-5">
+              <v-btn color="success"
+                     rounded
+                     x-large
+                     class="pa-5"
+                     to="/signin">
+                Get Started
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+export default {
+  name: 'Home'
+}
 </script>
