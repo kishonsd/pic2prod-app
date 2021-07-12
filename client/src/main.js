c7312433c77b7041277395c536278f9089bfdfa5
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-07-13 01:13:04 +0800

    main entry updated firebase + dynamic layotus

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
+import store from './store'
 import App from './App.vue'
 import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
 import firebase from 'firebase'
+import vuetify from './plugins/vuetify'
+import PageLayout from '@/layouts/PageLayout'
+import EmptyLayout from '@/layouts/EmptyLayout'
+import DashboardLayout from '@/layouts/DashboardLayout'
 
+/* Firebase */
 firebase.initializeApp({
   apiKey: "AIzaSyAXuA3cBjwxkZjh0iTt36yfJhCplH9EGzg",
   authDomain: "pic2prod-17012.firebaseapp.com",
@@ -15,7 +19,13 @@ firebase.initializeApp({
   appId: "1:439036061880:web:5f81c3ddeef2170c372c4d"
 })
 
-Vue.config.productionTip = false
+/* Layouts */
+Vue.component('page-layout', PageLayout)
+Vue.component('empty-layout', EmptyLayout)
+Vue.component('dashboard-layout', DashboardLayout)
+
+
+Vue.config.productionTip = true
 
 new Vue({
   router,
