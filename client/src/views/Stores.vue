28135273cc5e45cf9e573bb4a4c0a0e2eb2d17a2
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-07-13 01:12:05 +0800

    stores view

---
+++ client/src/views/Stores.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-main class="primary">
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <v-card class="mx-auto"
+                  color="transparent"
+                  flat
+                  shaped
+                  dark>
+            <v-card-text>
+              <h1 class="display-3 font-weight-bold white--text">
+                Connect your stores with a few clicks.
+              </h1>
+            </v-card-text>
+            <v-card-actions class="mt-5">
+              <v-btn color="success"
+                     rounded
+                     x-large
+                     class="pa-5"
+                     to="/signin">
+                Add Product
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
