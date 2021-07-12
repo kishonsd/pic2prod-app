80078bfa1b62fd2b1aaad4fc1297b42eeda49b6f
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-07-13 01:11:10 +0800

    added new store scripts

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import user from './scripts/user'
+import products from './scripts/products'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    user,
+    products
   }
 })
