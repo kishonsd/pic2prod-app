1183d3f7200dd2c17a15700c4184292d3ed2a055
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-07-12 20:59:24 +0800

    firebase init testing zone

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import firebase from 'firebase'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAXuA3cBjwxkZjh0iTt36yfJhCplH9EGzg",
+  authDomain: "pic2prod-17012.firebaseapp.com",
+  databaseURL: "https://pic2prod-17012.firebaseio.com",
+  projectId: "pic2prod-17012",
+  storageBucket: "pic2prod-17012.appspot.com",
+  messagingSenderId: "439036061880",
+  appId: "1:439036061880:web:5f81c3ddeef2170c372c4d"
+})
 
 Vue.config.productionTip = false
 
