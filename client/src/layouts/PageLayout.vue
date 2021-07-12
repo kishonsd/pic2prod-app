3396fd6949c7c2ff80bc273b5875686d3287de55
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-07-13 01:10:45 +0800

    page layout

---
+++ client/src/layouts/PageLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-app-navbar></v-app-navbar>
+    <slot />
+    <v-app-footer />
+  </v-app>
+</template>
+
+<script>
+import VAppNavbar from '@/components/VAppNavbar'
+import VAppFooter from '@/components/VAppFooter'
+
+export default {
+  name: 'PageLayout',
+  components: {
+    VAppNavbar,
+    VAppFooter
+  }
+}
+</script>
