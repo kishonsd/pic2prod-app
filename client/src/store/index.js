42717253e640cfae87b6791a9e0f0c15209630f7
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-07-15 22:00:34 +0800

    store states, mutations and actiosn

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './scripts/user'
 import products from './scripts/products'
+import stores from './scripts/stores'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     user,
-    products
+    products,
+    stores
   }
 })
