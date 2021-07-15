99cf0aeb0b3da697c4589abec6d0a9aeb899d82d
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-07-15 23:15:09 +0800

    post add hiboutik integration

--- client/src/components/VPostAdd.vue
+++ client/src/components/VPostAdd.vue
@@ -2,71 +2,105 @@
   <v-card color="transparent"
           flat
           dark
-          max-width="500"
+          max-width="400"
           class="mx-auto">
+    <v-card-title>
+      <v-img src="@/assets/Online shopping.png"></v-img>
+      <h1 class="display-1 font-weight-bold white--text">
+        Post
+      </h1>
+    </v-card-title>
     <v-card-text>
-      <v-img src="@/assets/Multimedia.png"></v-img>
-      <h1 class="title">Upload an image</h1>
-      <v-form v-model="uploadImageValid">
-        <v-file-input label=".jpg .jpeg .png"
-                      accept="image/*"
-                      v-model="product.image"
-                      required></v-file-input>
-        <v-checkbox label="AI remove background" />
+      <v-form>
+        <v-select v-model="post.productId"
+                  label="Product"
+                  :items="products.items"
+                  item-text="name"
+                  item-value="key"
+                  :rules="formRule.required" />
+
+        <v-select v-model="post.stores"
+                  label="Stores"
+                  :items="stores.items"
+                  item-text="name"
+                  return-object
+                  multiple
+                  :rules="formRule.required" />
       </v-form>
     </v-card-text>
 
-    <v-card-text>
-      <h1 class="title">Add basic details about your product</h1>
-      <v-form>
-        <v-text-field label="Name"
-                      type="text" />
-        <v-text-field label="Price"
-                      type="number" />
-      </v-form>
-    </v-card-text>
+    <div class="post-meta"
+         v-for="store in post.stores"
+         :key="store.key">
+      <v-card-title>
+        <h1 class="display-1 font-weight-bold white--text">
+          {{store.name}}
+        </h1>
+        <v-subheader>
+          {{store.platform}}
+        </v-subheader>
+      </v-card-title>
 
-    <v-card-text>
-      <h1 class="title">Select where to post</h1>
-      <v-form>
-        <v-checkbox label="Ebay" />
-        <v-checkbox label="Magento" />
-        <v-checkbox v-model="hiboutikSelected"
-                    label="Hiboutik" />
-        <v-checkbox label="Pretashop" />
-      </v-form>
-    </v-card-text>
-
-    <v-card-text v-if="hiboutikSelected">
-      <h1 class="title">Select what to post in Hiboutik</h1>
-      <v-form>
-        <v-text-field label="Product title" />
-      </v-form>
-    </v-card-text>
+      <v-card-text>
+        <component :is="handlePostStoreAdd(store.platform)"
+                   :store="store" />
+      </v-card-text>
+    </div>
 
     <v-card-actions>
-      <v-btn block
+      <v-btn @click="handleSubmit"
+             :disabled="!valid"
+             :loading="loading"
+             block
              rounded
-             class="pa-5"
              color="success"
-             :disabled="!valid">
-        Post
+             class="pa-5">
+        Submit
       </v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import firebase from 'firebase'
+import formRule from '@/utilities/formRule'
+import VPostHiboutikAdd from '@/components/VPostHiboutikAdd'
+
 export default {
+  name: 'VPostAdd',
+
+  components: {
+    VPostHiboutikAdd
+  },
+
   data: () => ({
-    uploadImageValid: true,
-    hiboutikSelected: false,
-    product: {}
+    formRule,
+    post: {},
+    valid: true,
+    loading: false
   }),
 
   computed: {
-    valid () {
-      return this.uploadImageValid
+    ...mapState([
+      'stores',
+      'products',
+      'user'
+    ])
+  },
+
+  methods: {
+    async handleSubmit () {
+      this.loading = true
+      await firebase.database()
+        .ref(`post/${this.user.me.uid}`)
+        .push(this.post)
+
+      this.post = {}
+      this.loading = false
+    },
+    handlePostStoreAdd (store) {
+      return `VPost${store}Add`
     }
   }
 }
