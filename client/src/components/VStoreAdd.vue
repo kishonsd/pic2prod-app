ce3f4b047e1b76a4cab62bf1a331db73c3e711e6
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-07-15 21:59:42 +0800

    add store linked to account

--- client/src/components/VStoreAdd.vue
+++ client/src/components/VStoreAdd.vue
@@ -1,108 +1,126 @@
 <template>
   <v-dialog v-model="dialog"
             max-width="500"
-            hide-overlay>
+            fullscreen>
+
     <template v-slot:activator="{ on, attrs }">
       <v-btn color="success"
              rounded
-             x-large
              class="pa-5"
              v-bind="attrs"
              v-on="on">
-        Connect Store
+        Add Store
       </v-btn>
     </template>
 
-    <v-card color="primary"
-            dark>
-      <v-card-title>
-        <v-img src="@/assets/Online shopping.png"></v-img>
-        <h1 class="display-1 font-weight-bold white--text">
-          Store
-        </h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form v-model="valid">
-          <v-file-input v-model="file"
-                        label="Image"
-                        accept="image/*" />
+    <v-sheet dark
+             color="primary"
+             class="fill-height">
+      <v-card color="transparent"
+              max-width="400"
+              class="mx-auto"
+              flat
+              dark>
+        <v-toolbar dark
+                   flat
+                   color="transparent">
+          <v-spacer></v-spacer>
+          <v-btn rounded
+                 text
+                 @click="dialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-toolbar>
 
-          <v-text-field v-model="product.name"
-                        label="Name"
-                        prepend-icon="mdi-basket" />
+        <v-card-title>
+          <v-img src="@/assets/Online shopping.png"></v-img>
+          <h1 class="display-1 font-weight-bold white--text">
+            Store
+          </h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form v-model="valid">
+            <v-text-field v-model="storeMeta.name"
+                          label="Name"
+                          type="text"
+                          :rules="formRule.required" />
 
-          <v-text-field v-model="product.price"
-                        label="Price"
-                        prepend-icon="mdi-cash" />
+            <v-select label="Select platform"
+                      v-model="platform"
+                      :items="platforms"
+                      item-text="text"
+                      item-value="value"
+                      :rules="formRule.required"></v-select>
 
-          <v-textarea v-model="product.description"
-                      label="Description"
-                      prepend-icon="mdi-text" />
-        </v-form>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn @click="handleSubmit"
-               :disabled="!valid"
-               :loading="loading"
-               color="success"
-               rounded
-               large>
-          Submit
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+            <component :is="platform"
+                       :credentials="credentials" />
+          </v-form>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn @click="handleSubmit"
+                 :disabled="!valid"
+                 :loading="loading"
+                 block
+                 rounded
+                 color="success"
+                 class="pa-5">
+            Submit
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-sheet>
   </v-dialog>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 import firebase from 'firebase'
-
+import formRule from '@/utilities/formRule'
+import VStoreHiboutikAdd from '@/components/VStoreHiboutikAdd'
 export default {
-  name: 'VProductAdd',
+  name: 'VStoreAdd',
+  components: {
+    VStoreHiboutikAdd
+  },
 
   data: () => ({
-    product: {},
-    valid: true,
-    loading: false,
+    formRule,
     dialog: false,
-    file: {}
+    valid: true,
+    platform: false,
+    platforms: [
+      { text: 'Hiboutik', value: VStoreHiboutikAdd }
+    ],
+    credentials: {},
+    loading: false,
+    storeMeta: {}
   }),
 
   computed: {
-    ...mapState([
-      'user'
-    ])
+    ...mapState('user', ['me']),
+    computePlatform () {
+      if (this.platform) return this.platforms.find(pl => pl.value === this.platform)
+      else return false
+    }
   },
 
   methods: {
     async handleSubmit () {
       this.loading = true
-      const uid = this.user.me.uid
-
-      const childRef = `products/${uid}/${this.file.name}`
-
-      await firebase.storage().ref()
-        .child(childRef)
-        .put(this.file)
-
-      const imageUrl = await firebase.storage().ref()
-        .child(childRef)
-        .getDownloadURL()
-
       await firebase.database()
-        .ref(`products/${this.user.me.uid}`)
+        .ref(`stores/${this.me.uid}`)
         .push({
-          ...this.product,
-          image: imageUrl
+          ...this.storeMeta,
+          platform: this.computePlatform.text,
+          credentials: this.credentials
         })
-
-      await this.$store.dispatch('products/loadList')
-
-      this.file = {}
-      this.product = {}
-      this.dialog = false
+      await this.$store.dispatch('stores/loadItems')
+      this.platform = false
+      this.credentials = {}
+      this.storeMeta = {}
       this.loading = false
+      this.dialog = false
     }
   }
 }
