d3880dfea4c76ababd8e6ca7d7ebe60d83f0eac5
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-07-16 05:47:24 +0800

    platform hiboutik

---
+++ backend/src/platforms.js
@@ -0,0 +1,33 @@
+const axios = require('axios')
+const logger = require('./utilities/logger')('platforms')
+const FormData = require('form-data')
+
+async function Hiboutik (credentials, product, meta) {
+    logger.info('post hiboutik')
+    const product_ = {}
+    for (let key in product) {
+        if (key === 'name') product_['product_model'] = product[key]
+        if (key === 'price') product_['product_price'] = product[key]
+    }
+
+    const form = new FormData()
+    Object.keys(meta).forEach(k => form.append(k, meta[k]))
+    Object.keys(product_).filter(k => !Object.keys(meta).includes(k)).forEach(k => form.append(k, product_[k]))
+
+    return axios({
+        url: `${credentials.domain}/api/products`,
+        method: 'POST',
+        data: form,
+
+        auth: {
+            username: credentials.username,
+            password: credentials.password
+        }
+    })
+        .then(response => [response.data, false])
+        .catch(err => [false, err])
+}
+
+module.exports = {
+    Hiboutik
+}
