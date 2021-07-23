545621463c9912fc76f3e1d36e3100e8ff09f794
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-07-23 14:39:47 +0800

    magento add fields basic magento integration #5

---
+++ client/src/components/VPostMagentoAdd.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="v-post-magento-add">
+    <v-select label="Fields"
+              v-model="selected"
+              :items="fields"
+              multiple
+              :rules="formRules.required"></v-select>
+  </div>
+</template>
+
+<script>
+import formRules from '@/mixins/formRules'
+
+export default {
+  name: 'VPostMagentoAdd',
+  mixins: [formRules],
+  data: () => ({
+    selected: [],
+    fields: [
+      "sku",
+      "name",
+      "attribute_set_id",
+      "price",
+      "status",
+      "visibility",
+      "type_id",
+      "weight",
+    ]
+  })
+}
+</script>
