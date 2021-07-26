54897189286f4408d8ae5da40320cc2571e7639d
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-07-26 14:49:47 +0800

    db lib post set

--- backend/lib/database.js
+++ backend/lib/database.js
@@ -6,3 +6,12 @@ async function postsGet () {
         .once('value')
         .then(snapshot => snapshot.val())
 }
+
+async function postSet (userId, postId, stores) {
+    const ref = `post/${userId}/${postId}/stores`
+    logger.info(`posts.set => ${ref}`)
+    return db
+        .ref(ref)
+        .set(stores)
+
+}
