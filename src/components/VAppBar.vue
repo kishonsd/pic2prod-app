1f7cf7dfefa931e05160137d8dd286babaff787d
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-12-06 10:13:36 +0800

    vappbar

---
+++ src/components/VAppBar.vue
@@ -0,0 +1,14 @@
+<template>
+	<v-sheet style="margin-bottom: 5em">
+		<v-container>
+			<v-app-bar dense flat color="white">
+				<v-app-bar-title class="font-weight-bold" style="letter-spacing: 0.1em">
+					<v-icon class="primary--text">mdi-file-powerpoint</v-icon>Pic2prod
+				</v-app-bar-title>
+				<v-spacer></v-spacer>
+				<v-btn text>Guide</v-btn>
+				<v-btn text>Dashboard</v-btn>
+			</v-app-bar>
+		</v-container>
+	</v-sheet>
+</template>
