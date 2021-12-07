c9f33bebf2c85921b7bc164b4df5a3ec26fee85e
kishonsd <kishonmgmt@gmail.com>
2021-12-07 14:33:01 -0800

    created VBtnSignin.vue

---
+++ src/views/VBtnSignin.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row
+    align="center"
+    justify="space-around"
+  >
+    <v-btn depressed>
+      Sign In
+    </v-btn>
+    <v-btn
+      depressed
+      color="primary"
+      rounded
+      elevation="6"
+      outlined
+     >
+     
+    </v-row>
+</template>
