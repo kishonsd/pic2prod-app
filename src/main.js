acd54fea80b71e0817b4dba2f01458294c8d2218
kishonsd <kishonmgmt@gmail.com>
2021-12-17 13:58:04 -0800

    signup with firebase authentication

--- src/main.js
+++ src/main.js
@@ -13,3 +13,15 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDaeFuzKG-qJ8Umq6bBpXBXDgxtZ6KsxLE",
+  authDomain: "pic2prod-dad7a.firebaseapp.com",
+  projectId: "pic2prod-dad7a",
+  storageBucket: "pic2prod-dad7a.appspot.com",
+  messagingSenderId: "77910974071",
+  appId: "1:77910974071:web:0002cf1d06b9b55fe33072",
+  measurementId: "G-W1BQ9XQVN2"
+  
+};
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,114 +1,87 @@
 <template>
-    <v-col
-          cols="5"
-          sm="6"
-          md="3"
-          >
-<v-container>
-  <validation-observer
-    ref="observer"
-    v-slot="{ invalid }"
-  >
-    <form @submit.prevent="submit">
-      <validation-provider
-        v-slot="{ errors }"
-        name="Username"
-        rules="required|max:15"
-      >
-        <v-text-field
-          v-model="name"
-          :counter="15"
-          :error-messages="errors"
-          label="Username"
-          required
-        ></v-text-field>
-      </validation-provider>
-
-      <validation-provider
-        v-slot="{ errors }"
-        name="email"
-        rules="required|email"
-      >
-        <v-text-field
-          v-model="email"
-          :error-messages="errors"
-          label="E-mail"
-          required
-        ></v-text-field>
-      </validation-provider>
-
-      <v-btn
-        class="mr-4"
-        type="submit"
-        :disabled="invalid"
-        >
-        submit
-      </v-btn>
-      <v-btn @click="clear">
-        Cancel
-      </v-btn>
-    </form>
-  </validation-observer>
-</v-container>
-</v-col>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <form @submit.prevent="onSignup">
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="email"
+                      label="E-mail"
+                      id="email"
+                      v-model="email"
+                      type="email"
+                      required></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="password"
+                      label="Password"
+                      id="password"
+                      v-model="password"
+                      type="password"
+                      required></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="confirmPassword"
+                      label="Confirm Password"
+                      id="confirmPassword"
+                      v-model="confirmPassword"
+                      type="password"
+                      :rules="[comparePasswords]"></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-btn type="submit">Sign up</v-btn>
+                  </v-flex>
+                </v-layout>
+              </form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-  import { required, digits, email, max, regex } from 'vee-validate/dist/rules'
-  import { extend, ValidationObserver, ValidationProvider, setInteractionMode } from 'vee-validate'
-
-  setInteractionMode('eager')
-
-  extend('digits', {
-    ...digits,
-    message: '{_field_} needs to be {length} digits. ({_value_})',
-  })
-
-  extend('required', {
-    ...required,
-    message: '{_field_} can not be empty',
-  })
-
-  extend('max', {
-    ...max,
-    message: '{_field_} may not be greater than {length} characters',
-  })
-
-  extend('regex', {
-    ...regex,
-    message: '{_field_} {_value_} does not match {regex}',
-  })
-
-  extend('email', {
-    ...email,
-    message: 'Email must be valid',
-  })
 
   export default {
-    components: {
-      ValidationProvider,
-      ValidationObserver,
+    data () {
+      return {
+        email: '',
+        password: '',
+        confirmPassword: ''
+      }
     },
-    data: () => ({
-      name: '',
-      email: '',
-      select: null,
-      items: [
-        'Item 1',
-        'Item 2',
-      ],
-      checkbox: null,
-    }),
-
+    computed: {
+      comparePasswords () {
+        return this.password !== this.confirmPassword ? 'Passwords do not match' : ''
+      },
+      user () {
+        return this.$store.getters.user
+      }
+    },
+    watch: {
+      user (value) {
+        if (value !== null && value !== undefined) {
+          this.$router.push('/VFormProduct')
+        }
+      }
+    },
     methods: {
-      submit () {
-        this.$refs.observer.validate()
-      },
-      clear () {
-        this.name = ''
-        this.email = ''
-        this.$refs.observer.reset()
-      },
-    },
+      onSignup () {
+        this.$store.dispatch('signUserUp', {email: this.email, password: this.password})
+      }
+    }
   }
 </script>
